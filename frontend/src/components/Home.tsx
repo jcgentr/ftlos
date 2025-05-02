8299a34de4f414d462ac9bcd6604cd143901f0e5
Jared Gentry <jared.c.gentry@gmail.com>
2025-05-02 17:13:05 -0400

    starts home page

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,8 +1,45 @@
+import { NavLink } from "react-router";
+import { Button } from "./ui/button";
+import { Star, UserPen } from "lucide-react";
+import { TableDemo } from "./RateTable";
+
 export function Home() {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-4">Home Page</h1>
-      <p>Are you the only person in the world who...</p>
+    <div className="p-8 max-w-5xl ml-auto mr-auto">
+      <h1 className="text-5xl font-bold text-primary">Are you the only person in the world who...</h1>
+      <p>You aren't alone anymore! Find your new best friend here!</p>
+      {/* Connect with Fans */}
+      <div className="border border-accent mt-8">
+        <div className="flex justify-between items-center p-4 border-b border-accent">
+          <h2 className="text-2xl font-bold">Connect with Fans</h2>
+          <NavLink to="/fans">
+            <Button variant="outline">Find More Fans</Button>
+          </NavLink>
+        </div>
+        <div className="flex flex-col gap-2 justify-center items-center py-4">
+          <p>We'll show personalized fan recommendations here once you:</p>
+          <p className="flex items-center gap-2 mt-4">
+            <UserPen /> Complete your profile with interests
+          </p>
+          <p className="flex items-center gap-2">
+            <Star /> Rate some sports entities
+          </p>
+          <NavLink to="/fans" className="mt-4">
+            <Button>Browse All Fans</Button>
+          </NavLink>
+        </div>
+      </div>
+      {/* Rate These Sports Entities */}
+      <div className="border border-accent mt-8">
+        <div className="p-4 border-b border-accent">
+          <h2 className="text-2xl font-bold">Rate These Sports Entities</h2>
+          <p>Help us learn your preferences! Rate from 1 (strongly dislike) to 5 (strongly like)</p>
+        </div>
+        <div className="flex flex-col gap-2 justify-center items-center py-4">
+          <TableDemo />
+          <Button className="mt-4">Get New Entitites to Rate</Button>
+        </div>
+      </div>
     </div>
   );
 }
